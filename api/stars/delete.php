f18674e1ddcd2bceb169ae741215ed00c89ed18a
gabor2 <gabor2@macs-MacBook-Pro.local>
2023-01-02 22:21:12 +0100

    add auth check

--- api/stars/delete.php
+++ api/stars/delete.php
@@ -7,3 +7,10 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
 include_once '../../config/database.php';
 include_once '../../models/star.php';
 include_once '../../models/api-key.php';
+
+
+
+$isAuth = $apiKey->authenticate($headers);
+
+if (!$isAuth)
+    return;
