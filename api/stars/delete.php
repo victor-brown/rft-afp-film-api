2d05d8e01fa28bcba9779f983fe04b201a09e50f
gabor2 <gabor2@macs-MacBook-Pro.local>
2023-01-02 22:20:27 +0100

    Stars delete basic setup

---
+++ api/stars/delete.php
@@ -0,0 +1,9 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: DELETE');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods');
+
+include_once '../../config/database.php';
+include_once '../../models/star.php';
+include_once '../../models/api-key.php';
