a8ef4a3ecc88d998ac6efabddc5da8d6ad13550d
gabor2 <gabor2@macs-MacBook-Pro.local>
2023-01-02 23:04:20 +0100

    add genres delete functionality

--- api/genres/delete.php
+++ api/genres/delete.php
@@ -7,3 +7,33 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
 include_once '../../config/database.php';
 include_once '../../models/genre.php';
 include_once '../../models/api-key.php';
+
+
+
+
+$database = new Database();
+$db = $database->connect();
+$apiKey = new ApiKey($db);
+
+$headers = apache_request_headers();
+$isAuth = $apiKey->authenticate($headers);
+
+if (!$isAuth)
+    return;
+
+
+$genre = new Genre($db);
+
+$genre->id = isset($_GET['id']) ? $_GET['id'] : die();
+
+
+if ($genre->delete()) {
+    echo json_encode(
+        array('deleted' => $genre->id)
+    );
+    return;
+} else {
+    http_response_code(400);
+    echo json_encode(array('message' => 'Bad Request'));
+    return;
+}
