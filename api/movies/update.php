16526cfce2cc3f63dfa1a4fb70ab30b05c1062b8
gabor2 <gabor2@macs-MacBook-Pro.local>
2023-01-02 23:38:04 +0100

    add movies update file, headers and includes

---
+++ api/movies/update.php
@@ -0,0 +1,9 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: PUT');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../../config/database.php';
+include_once '../../models/movie.php';
+include_once '../../models/api-key.php';
