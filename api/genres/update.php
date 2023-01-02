82be1c6adf543efa19421ac195589573d577d81c
gabor2 <gabor2@macs-MacBook-Pro.local>
2023-01-02 23:26:58 +0100

    add genres update funcitons

--- api/genres/update.php
+++ api/genres/update.php
@@ -9,3 +9,29 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
 include_once '../../config/database.php';
 include_once '../../models/genre.php';
 include_once '../../models/api-key.php';
+
+$database = new Database();
+$db = $database->connect();
+$apiKey = new ApiKey($db);
+$headers = apache_request_headers();
+$isAuth = $apiKey->authenticate($headers);
+
+if (!$isAuth)
+    return;
+
+$data = json_decode(file_get_contents("php://input"));
+
+$genre = new Genre($db);
+$genre->id = isset($_GET['id']) ? $_GET['id'] : die();
+$genre->name = $data->name;
+
+if ($genre->update()) {
+    echo json_encode(
+        array('updated' => $genre->id)
+    );
+    return;
+} else {
+    http_response_code(400);
+    echo json_encode(array('message' => 'Bad Request'));
+    return;
+}
