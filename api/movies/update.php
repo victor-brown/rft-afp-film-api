55ef50418d32ce38399305e0bc2497c223b3b93a
gabor2 <gabor2@macs-MacBook-Pro.local>
2023-01-02 23:45:08 +0100

    add movies update function

--- api/movies/update.php
+++ api/movies/update.php
@@ -7,3 +7,33 @@ header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
 include_once '../../config/database.php';
 include_once '../../models/movie.php';
 include_once '../../models/api-key.php';
+
+$database = new Database();
+$db = $database->connect();
+$apiKey = new ApiKey($db);
+$headers = apache_request_headers();
+$isAuth = $apiKey->authenticate($headers);
+
+if (!$isAuth)
+    return;
+
+$data = json_decode(file_get_contents("php://input"));
+
+$movie = new Movie($db);
+$movie->id = isset($_GET['id']) ? $_GET['id'] : die();
+$movie->title = $data->title;
+$movie->description = $data->description;
+$movie->image_url = $data->image_url;
+$movie->certificate = $data->certificate;
+$movie->imdb_rating = $data->imdb_rating;
+
+if ($movie->update()) {
+    echo json_encode(
+        array('updated' => $movie->id)
+    );
+    return;
+} else {
+    http_response_code(400);
+    echo json_encode(array('message' => 'Bad Request'));
+    return;
+}
